21c5e7d069fbdff91d3ad832cd5b51428e21b676
Rodrigo dos Santos <rodrigo.contato@live.com>
2021-05-18 18:10:14 -0300

    API Count

--- Backend/src/api/billingCycle/billingCycleService.js
+++ Backend/src/api/billingCycle/billingCycleService.js
@@ -4,21 +4,23 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
 BillingCycle.route('get', (req, res, next) => {
-
     BillingCycle.find({}, (err, docs) => {
-
         if(!err) {
-
             res.json(docs)
-
         } else {
-
             res.status(500).json({errors: [error]})
-
         }
-
     })
+})
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count({}, (error, value) => {
+        if(!error) {
+            res.json({value})
+        } else {
+            res.status(500).json({errors: [error]})
+        }
+    })
 })
 
 module.exports = BillingCycle
