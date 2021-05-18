6d51234088eedaba27b60a9f276b64926f047876
Rodrigo dos Santos <rodrigo.contato@live.com>
2021-05-18 18:26:32 -0300

    Summary

--- Backend/src/api/billingCycle/billingCycleService.js
+++ Backend/src/api/billingCycle/billingCycleService.js
@@ -5,20 +5,36 @@ BillingCycle.updateOptions({ new: true, runValidators: true })
 
 BillingCycle.route('get', (req, res, next) => {
     BillingCycle.find({}, (err, docs) => {
-        if(!err) {
+        if (!err) {
             res.json(docs)
         } else {
-            res.status(500).json({errors: [error]})
+            res.status(500).json({ errors: [error] })
         }
     })
 })
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count({}, (error, value) => {
-        if(!error) {
-            res.json({value})
+        if (!error) {
+            res.json({ value })
         } else {
-            res.status(500).json({errors: [error]})
+            res.status(500).json({ errors: [error] })
+        }
+    })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+    }, {
+        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+    }, {
+        $project: { _id: 0, credit: 1, debt: 1 }
+    }], (error, result) => {
+        if (error) {
+            res.status(500).json({ errors: [error] })
+        } else {
+            res.json(result[0] || { credit: 0, debt: 0 })
         }
     })
 })
